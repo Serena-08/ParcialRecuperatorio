d58edc14754ea5c825796ae5bfb8f1ea87c8cdce
Serena <serefisch17@gmail.com>
2025-08-13 20:01:13 -0300

    Recuperatorio

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
   public static void main(String[] args) {
 
 
-    Android celular1 = new Android ("132456", "Samsung S23 Ultra", 2024, 2,500,000);
-    Iphone celuar2 = new Iphone ("0909998", "Iphone 13", 2022, 1,500,00);
-    Android celular3 = new Android ("827910", "Samsung S24", 2024, 2,350,00);
+    Android celular1 = new Android ("132456", "Samsung S23 Ultra", 2024, 2500000);
+    Iphone celuar2 = new Iphone ("0909998", "Iphone 13", 2022, 1);
+    Android celular3 = new Android ("827910", "Samsung S24", 2024, 235000);
 
 
 
--- src/model/Smartphones/Iphone.java
+++ src/model/Smartphones/Iphone.java
@@ -1,11 +1,15 @@
 package model.Smartphones;
 
 public class Iphone extends Smartphone {
-
   private Smartphone smartphone;
 
   public Iphone(String nroSerie, String modelo, int anio, float costoBase) {
     super(nroSerie, modelo, anio, costoBase);
+  }
+
+  @Override
+  public String getTipo() {
+    return "";
   }
 
   public Smartphone getSmartphone() {
