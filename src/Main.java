7e74e14061103ca37ce6cfc16929691eb5f542aa
Serena <serefisch17@gmail.com>
2025-08-13 20:05:36 -0300

    Recuperatorio

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
   public static void main(String[] args) {
 
 
-    Android celular1 = new Android ("132456", "Samsung S23 Ultra", 2024, 2500000);
-    Iphone celuar2 = new Iphone ("0909998", "Iphone 13", 2022, 1);
-    Android celular3 = new Android ("827910", "Samsung S24", 2024, 235000);
+    Smartphone celular1 = new Android ("132456", "Samsung S23 Ultra", 2024, 2500000);
+    Smartphone celuar2 = new Iphone ("0909998", "Iphone 13", 2022, 1);
+    Smartphone celular3 = new Android ("827910", "Samsung S24", 2024, 235000);
 
 
 
